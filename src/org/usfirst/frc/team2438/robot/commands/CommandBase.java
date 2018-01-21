6553d60aef3951eaf16f5185853bb5342c7a106f
Robotics <ewh1901@iolani.org>
2018-01-20 17:23:00 -1000

    initial code

---
+++ src/org/usfirst/frc/team2438/robot/commands/CommandBase.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team2438.robot.commands;
+
+import org.usfirst.frc.team2438.robot.OI;
+import org.usfirst.frc.team2438.robot.subsystems.Lift;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public abstract class CommandBase extends Command {
+	public static OI oi = new OI();
+	public static Lift lift = new Lift();
+	
+	public static void init() {
+		lift.init();
+	}
+}
